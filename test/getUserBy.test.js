9a19eafeb77bf77a2f71a09a145c49547b73066e
kihdae <148399054+kihdae@users.noreply.github.com>
2025-01-26 18:06:32 -0600

    test case 1 should return a user by ID

--- test/getUserBy.test.js
+++ test/getUserBy.test.js
@@ -11,4 +11,15 @@ describe("getUserByIdService", () => {
     beforeEach(() => {
         jest.clearAllMocks();
     });
+
+it("should return a user by ID", async () => {
+    const mockUserId = 1;
+    const mockUser = { id: 1, username: "testuser" };
+    db.users.findUnique.mockResolvedValueOnce(mockUser); 
+
+    const result = await getUserByIdService(mockUserId);
+
+    expect(db.users.findUnique).toHaveBeenCalledWith({ where: { id: mockUserId } });
+    expect(result).toEqual(mockUser);
+  });
 })
