138f464b4a7546a9dd9857100f1a56c9a9b98bdc
kihdae <148399054+kihdae@users.noreply.github.com>
2025-01-28 01:34:35 -0600

    added to set up

--- test/loginService.test.js
+++ test/loginService.test.js
@@ -2,18 +2,21 @@ import { describe, it, beforeEach } from "mocha";
 import { expect } from "chai";
 import sinon from "sinon";
 import db from "../libs/prismaInit.js";
-import { loginService } from "../services/loginService.js";
+import { generateAuthToken } from "../utilities/utilities.js"; 
+import bcrypt from "bcrypt";
+import { loginUserService } from "../services/loginService.js";
 
-describe("loginService", () => {
+describe("loginUserService", () => {
   let sx;
 
   before(() => {
     sx = sinon.createSandbox();
 
-    sx.stub(db, "users").value({});
+    sx.stub(db.users, "findUnique").resolves(null);//so that it defaults to no user found
+    sx.stub(bcrypt.compare).returns(true);//default to success on pw comparison
   });
 
   afterEach(() => {
     sx.restore();
   });
-});
+});   
